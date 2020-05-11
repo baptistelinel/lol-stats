5d2c496ffccad92dc73e58534c961af8e1c9cf17
Baptiste LINEL <blinel.public@gmail.com>
2020-05-11 11:19:00 +0200

    [ADD] format also applied on tests/

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ connect_to_database:
 	docker exec -it database psql lol-stats user
 
 format:
-	yapf -i -r sources/ --style='{based_on_style: pep8, indent_width: 4}'
+	yapf -i -r sources/ tests/ --style='{based_on_style: pep8, indent_width: 4}'
 
 unit_tests:
 	PYTHONPATH=sources pytest
--- tests/unit_tests/requesters/test_league_requester.py
+++ tests/unit_tests/requesters/test_league_requester.py
@@ -6,12 +6,17 @@ from requesters.league_requester import LeagueRequester
 
 
 class TestLeagueRequester(unittest.TestCase):
-	def setUp(self) -> None:
-		self._requests_adapter = Mock(spec=RequestsAdapter)
-		self._league_requester = LeagueRequester(self._requests_adapter)
+    def setUp(self) -> None:
+        self._requests_adapter = Mock(spec=RequestsAdapter)
+        self._league_requester = LeagueRequester(self._requests_adapter)
 
-	def test_get_by_id(self):
-		self._requests_adapter.http_get.return_value = {'json': {'summoner_id': 'summoner_id_123'}}
-		response = self._league_requester.get_by_id('summoner_id_123')
-		self._requests_adapter.http_get.assert_called_once_with(f'league/v4/entries/by-summoner/summoner_id_123')
-		self.assertEqual(response, {'summoner_id': 'summoner_id_123'})
+    def test_get_by_id(self):
+        self._requests_adapter.http_get.return_value = {
+            'json': {
+                'summoner_id': 'summoner_id_123'
+            }
+        }
+        response = self._league_requester.get_by_id('summoner_id_123')
+        self._requests_adapter.http_get.assert_called_once_with(
+            f'league/v4/entries/by-summoner/summoner_id_123')
+        self.assertEqual(response, {'summoner_id': 'summoner_id_123'})
