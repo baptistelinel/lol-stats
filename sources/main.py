47b0974d988d95da2c9e75f179cf00c1585a85e9
Baptiste LINEL <blinel.public@gmail.com>
2020-05-14 15:19:33 +0200

    [ADD] flask implementation with cache

--- sources/main.py
+++ sources/main.py
@@ -1,25 +1,26 @@
+from flask import Flask
+from flask_caching import Cache
+
 from adapters.requests_adapter import RequestsAdapter
-from business.win_rate_business import WinRateBusiness
-from requesters.league_requester import LeagueRequester
-from requesters.match_requester import MatchRequester
+from requesters.summoner_requester import SummonerRequester
+
+app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+requests_adapter = RequestsAdapter()
 
 
-class Main:
-    def __init__(self, win_rate_business: WinRateBusiness):
-        self._win_rate_business = win_rate_business
+@app.route('/')
+def home():
+    return 'Home'
 
-    def run(self):
-        match_id = [
-            '4594008545', '4593809699', '4593862618', '4592000594',
-            '4591938736'
-        ]
-        result = self._win_rate_business.get_from_match_history(
-            'Lets Coin Flip', match_id)
-        print(result)
+
+@app.route('/match-details')
+@cache.cached(timeout=50, key_prefix='match_details')
+def match_details():
+    summoner_requester = SummonerRequester(requests_adapter)
+    summoner = summoner_requester.get_by_name('Lets Coin Flip')
+    return summoner
 
 
 if __name__ == '__main__':
-    requests_adapter = RequestsAdapter()
-    league_requester = LeagueRequester(requests_adapter)
-    match_requester = MatchRequester(requests_adapter)
-    Main(WinRateBusiness(league_requester, match_requester)).run()
+    app.run(debug=True)
